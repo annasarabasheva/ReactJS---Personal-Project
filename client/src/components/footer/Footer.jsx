029828c9303b5e65dc84cc4c48b5faf41f89100d
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-17 16:39:56 +0300

    Add different style to the footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,11 +4,23 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
-            <p><FontAwesomeIcon icon={faCopyright} /> 2024 - All rights reserved</p>
-            <p><FontAwesomeIcon icon={faEarthEurope} /> Artists from all over the world</p>
-            <p><FontAwesomeIcon icon={faPalette} /> Your artwork is secure and cherished here</p>
-            <p><FontAwesomeIcon icon={faHeart} /> Unleash your creativity with us</p>
+        <footer>
+            <div className={styles.container}>
+                <div className={styles.fav}>
+                    <p><FontAwesomeIcon icon={faEarthEurope} /> Artists from all over the world</p>
+                    <p><FontAwesomeIcon icon={faPalette} /> Your artwork is secure and cherished here</p>
+                    <p><FontAwesomeIcon icon={faHeart} /> Unleash your creativity with us</p>
+                    <p><FontAwesomeIcon icon={faCopyright} /><strong>2024 - All rights reserved</strong></p>
+                </div>
+                <div className={styles.icons}>
+                    <p><img src="/images/IconOne.jpg" /></p>
+                    <p><img src="/images/IconTwo.jpg"/></p>
+                    <p><img src="/images/IconThree.jpg"/></p>
+                    <p><img src="/images/IconFour.jpg"/></p>
+                </div>
+            </div>
+             
         </footer>
+           
     );
 }
