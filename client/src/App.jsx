7ab7a72ef23cdad4e9806bd8819dcbf411ab6910
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-18 15:20:32 +0300

    Add static Gallery component and Gallery css cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
 import {Route, Routes} from 'react-router-dom'
 import About from './components/about/About';
+import Gallery from './components/gallery/Gallery';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
+                <Route path="/gallery" element={<Gallery/>}/>
         
             </Routes>
 
