43b0bf8de528c7d50a70abe0a8bc3ad1509b5ce1
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-30 18:28:54 +0300

    Add edit and delete buttons, like button and comments section

--- client/src/components/art-details/ArtDetails.jsx
+++ client/src/components/art-details/ArtDetails.jsx
@@ -1,4 +1,6 @@
-import styles from './ArtDetails.module.css'
+import styles from './ArtDetails.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function ArtDetails() {
@@ -8,13 +10,40 @@ export default function ArtDetails() {
             <div className={styles.artBox}>
                 <img src="/images/FlowerGirl.jpeg" alt="title" />
                 <div className={styles.info}>
-                    <p>Title: <span> Flowers in her eyes</span></p>
-                    <p>Category: <span> Portrait</span></p>
-                    <p>Description: <span> Spring in the eyes of young girl</span></p>
+                    <div className={styles.text}>
+                        <p>Title: <span> Flowers in her eyes</span></p>
+                        <p>Created By: <span> annabanana</span></p>
+                        <p>Category: <span> Portrait</span></p>
+                        <p>Description: <span> Spring in the eyes of young girl</span></p>
+                    </div>
+                    <div className={styles.commentSection}>
+                        <h3>Comment Section</h3>
+                        <ul className={styles.comments}>
+                            <li>Nice</li>
+                            <li>Amazingly drawns bravooo</li>
+                            <li>Incredible</li>
+                            <li>You are an amazing artist</li>
+                        </ul>
+                    </div>
+
+                    {/* VISIBLE ONLY FOR THE CREATOR
+                    <div className={styles.buttons}>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div> 
+                    */}
+
+                    {/* VISIBLE FOR LOGGED-IN USERS*/}
+                    <div className={styles.extras}>
+                        <button>5 <FontAwesomeIcon icon={faHeart} className={styles.iconStyle}/></button>
+                        <button className={styles.comment}>Add a Comment</button>
+                        
+                    </div> 
+                    
+                    
+                    
                 </div>
-            
             </div>
-            
         </div>
     );
 }
