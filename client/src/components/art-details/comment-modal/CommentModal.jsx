4b90e7d8b1b306b6387c427ddccac55f3dcb6730
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-31 17:16:22 +0300

    Add custom css style for the commentModal

--- client/src/components/art-details/comment-modal/CommentModal.jsx
+++ client/src/components/art-details/comment-modal/CommentModal.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import styles from './CommentModal.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function CommentModal({ isVisible, onClose, onSubmit }) {
     const [comment, setComment] = useState("");
@@ -9,7 +11,7 @@ export default function CommentModal({ isVisible, onClose, onSubmit }) {
     const handleSubmit = (e) => {
         e.preventDefault();
         onSubmit(comment);
-        setComment("");  // Clear the textarea after submission
+        setComment("");
     };
 
     const handleChange = (e) => {
@@ -19,18 +21,20 @@ export default function CommentModal({ isVisible, onClose, onSubmit }) {
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modal}>
-                <button className={styles.closeButton} onClick={onClose}>X</button>
+                <button className={styles.closeButton} onClick={onClose}><FontAwesomeIcon icon={faXmark} /></button>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="comment">Your Comment:</label>
+                    <label htmlFor="comment">Add a comment:</label>
                     <textarea 
                         id="comment" 
                         name="comment" 
-                        rows="10" 
+                        rows="7" 
                         value={comment}
                         onChange={handleChange}
                         required
+                        placeholder="Give your feedback here..." 
+                        style={{ fontSize: '35px' }}
                     ></textarea>
-                    <button type="submit">Submit</button>
+                <button type="submit">Submit</button>
                 </form>
             </div>
         </div>
