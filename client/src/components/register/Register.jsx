7e2355d415b5e895106149787c2fde2526d065ba
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-08-02 14:45:09 +0300

    Make register form controlled with useForm

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,30 +1,51 @@
 import styles from './Register.module.css'
 import { Link } from 'react-router-dom';
+import { useContext } from "react";
+
+import AuthContext from "../../contexts/authContext";
+import useForm from "../../hooks/useForm";
+
+const RegisterFormKeys = {
+    Username: 'username',
+    Email: 'email',
+    Password: 'password',
+    ConfirmPassword: 'rePassword',
+};
+
 
 export default function Register() {
+
+    const { registerSubmitHandler } = useContext(AuthContext);
+    const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+        [RegisterFormKeys.Username]: '',
+        [RegisterFormKeys.Email]: '',
+        [RegisterFormKeys.Password]: '',
+        [RegisterFormKeys.ConfirmPassword]: '',
+    });
+
     return (
         <div className={styles.registerContainer}>
               <div className={styles.container}>
                 <h1>Register</h1> 
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className={styles.formGroup}>
                         <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" name="username" placeholder="Username.." />
+                        <input type="text" id="username" name={RegisterFormKeys.Username} placeholder="Username.." onChange={onChange} value={values[RegisterFormKeys.Username]}/>
                     </div>
 
                     <div className={styles.formGroup}>
                         <label htmlFor="email">Email:</label>
-                        <input type="text" id="email" name="email" placeholder="Email.." />
+                        <input type="text" id="email" name={RegisterFormKeys.Email} placeholder="Email.." onChange={onChange} value={values[RegisterFormKeys.Email]}/>
                     </div>
 
                     <div className={styles.formGroup}>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" placeholder="Password.." />
+                        <input type="password" id="password" name={RegisterFormKeys.Password} placeholder="Password.." onChange={onChange} value={values[RegisterFormKeys.Password]}/>
                     </div>
 
                     <div className={styles.formGroup}>
                         <label htmlFor="rePassword">Confirm Password:</label>
-                        <input type="password" id="rePassword" name="rePassword" placeholder="Repeat Password.." />
+                        <input type="password" id="rePassword" name={RegisterFormKeys.ConfirmPassword} placeholder="Repeat Password.." onChange={onChange} value={values[RegisterFormKeys.ConfirmPassword]}/>
                     </div>
 
                     <input type="submit" value="Submit" className={styles.submitButton} />
