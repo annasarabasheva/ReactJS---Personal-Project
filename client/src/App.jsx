b306bf6411243aa01f76a024b0e00f71fec76af2
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-08-02 17:10:43 +0300

    Insert authService logic in the authContext, and clear the App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,63 +1,21 @@
-import React, { useState, useEffect } from 'react';
+
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import About from './components/about/About';
 import Gallery from './components/gallery/Gallery';
 import ArtCreate from './components/art-create/ArtCreate';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 import ArtDetails from './components/art-details/ArtDetails';
-import AuthContext from './contexts/authContext';
-import * as authService from './services/authService';
+import {AuthProvider} from './contexts/authContext';
 import Logout from './components/logout/Logout';
 
 function App() {
-    const navigate = useNavigate();
-    const [auth, setAuth] = useState(() => {
-        localStorage.removeItem('accessToken');
-        return {};
-    });
-
-    const loginSubmitHandler = async (values) => {
-        try {
-            const result = await authService.login(values.email, values.password);
-            setAuth(result);
-            navigate('/');
-        } catch (error) {
-            console.error('Login failed', error);
-        }
-    };
-
-    const registerSubmitHandler = async (values) => {
-        try {
-            const result = await authService.register(values.email, values.password, values.username);
-            setAuth(result);
-            navigate('/');
-        } catch (error) {
-            console.error('Registration failed', error);
-        }
-    };
-
-    const logoutHandler = () => {
-        setAuth({});
-        localStorage.removeItem('accessToken');
-    };
-
-
-    const values = {
-        loginSubmitHandler,
-        registerSubmitHandler,
-        logoutHandler,
-        username: auth.username,
-        email: auth.email,
-        userID: auth._id,
-        isAuthenticated: !!auth.accessToken,
-    };
-
+    
     return (
-        <AuthContext.Provider value={values}>
+        <AuthProvider>
             <Header />
 
             <Routes>
@@ -72,7 +30,7 @@ function App() {
             </Routes>
 
             <Footer />
-        </AuthContext.Provider>
+        </AuthProvider>
     );
 }
 
--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,7 +1,65 @@
 import { createContext } from "react";
+import { useNavigate } from 'react-router-dom';
+import {useState} from 'react';
+
+import * as authService from '../services/authService';
 
 const AuthContext = createContext();
 
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem('accessToken');
+        return {};
+    });
+
+    const loginSubmitHandler = async (values) => {
+        try {
+            const result = await authService.login(values.email, values.password);
+            setAuth(result);
+            navigate('/');
+        } catch (error) {
+            console.error('Login failed', error);
+        }
+    };
+
+    const registerSubmitHandler = async (values) => {
+        try {
+            const result = await authService.register(values.email, values.password, values.username);
+            setAuth(result);
+            navigate('/');
+        } catch (error) {
+            console.error('Registration failed', error);
+        }
+    };
+
+    const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('accessToken');
+    };
+
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username,
+        email: auth.email,
+        userID: auth._id,
+        isAuthenticated: !!auth.accessToken,
+    };
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+
+};
 AuthContext.displayName = 'AuthContext';
 
 export default AuthContext;
