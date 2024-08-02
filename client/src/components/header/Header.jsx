ba6091ffe26e3d6f201d9750eef75bc623b6bf4e
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-08-02 14:15:02 +0300

    Fix Navigation when user is authenticated

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css'; 
+import { useContext } from 'react';
+import AuthContext from '../../contexts/authContext';
 
 export default function Header() {
+
+    const {
+        isAuthenticated
+    } = useContext(AuthContext);
+    
     return (
         <header className={styles.header}>
             <img
@@ -14,11 +21,24 @@ export default function Header() {
                     <li><Link className={styles.navLink} to="/">Home</Link></li>
                     <li><Link className={styles.navLink} to="/about">About Us</Link></li>
                     <li><Link className={styles.navLink} to="/gallery">Gallery</Link></li>
-                    <li><Link className={styles.navLink} to="/create">Share your Art</Link></li>
-                    <li><Link className={styles.navLink} to="/login">Login</Link></li>
-                    <li><Link className={styles.navLink} to="/logout">Logout</Link></li>
-                    <li><Link className={styles.navLink} to="/register">Register</Link></li>
                     <li><Link className={styles.navLink} to="/search">Search</Link></li>
+                    {isAuthenticated && (
+                        <>
+                            <li><Link className={styles.navLink} to="/create">Share your Art</Link></li>
+                            <li><Link className={styles.navLink} to="/logout">Logout</Link></li>
+                        </>
+                        
+                    )}
+                    {!isAuthenticated && (
+                        <>
+                            <li><Link className={styles.navLink} to="/login">Login</Link></li>
+                            <li><Link className={styles.navLink} to="/register">Register</Link></li>
+                        
+                        </>
+                    )}
+                    
+                   
+                   
                 </ul>
             </nav>
         </header>
