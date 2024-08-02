e0e1273db2d92293300c2bfa47ca3f9532bdea0e
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-08-02 19:20:59 +0300

    Add authGuards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
-
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import About from './components/about/About';
 import Gallery from './components/gallery/Gallery';
 import ArtCreate from './components/art-create/ArtCreate';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 import ArtDetails from './components/art-details/ArtDetails';
-import {AuthProvider} from './contexts/authContext';
+import { AuthProvider } from './contexts/authContext';
 import Logout from './components/logout/Logout';
+import AuthGuard from './guards/AuthGuard';
 
 function App() {
-    
     return (
         <AuthProvider>
             <Header />
@@ -22,12 +21,15 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/gallery" element={<Gallery />} />
                 <Route path="/gallery/:artID/details" element={<ArtDetails />} />
-                <Route path="/create" element={<ArtCreate />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/logout" element={<Logout />} />
+                
+                {/* Routes that require authentication */}
+                <Route element={<AuthGuard />}>
+                    <Route path="/create" element={<ArtCreate />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Route>
             </Routes>
-
             <Footer />
         </AuthProvider>
     );
