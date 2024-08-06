4fd45b404ac614fc82bac239a1b6bce7445e873f
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-08-06 15:44:22 +0300

    Add error validation for register page
    Like in login, here if user doesnt give data for some of the filed or passwords are not matching, a specific message appears

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,6 @@
-import styles from './Register.module.css'
+import styles from './Register.module.css';
 import { Link } from 'react-router-dom';
 import { useContext } from "react";
-
 import AuthContext from "../../contexts/authContext";
 import useForm from "../../hooks/useForm";
 
@@ -12,48 +11,109 @@ const RegisterFormKeys = {
     ConfirmPassword: 'rePassword',
 };
 
+const validate = (values) => {
+    const errors = {};
+
+    if (!values[RegisterFormKeys.Username]) {
+        errors[RegisterFormKeys.Username] = 'Username is required';
+    }
+
+    if (!values[RegisterFormKeys.Email]) {
+        errors[RegisterFormKeys.Email] = 'Email is required';
+    }
+
+    if (!values[RegisterFormKeys.Password]) {
+        errors[RegisterFormKeys.Password] = 'Password is required';
+    } 
+
+    if (!values[RegisterFormKeys.ConfirmPassword]) {
+        errors[RegisterFormKeys.ConfirmPassword] = 'Confirm Password is required';
+    } else if (values[RegisterFormKeys.Password] !== values[RegisterFormKeys.ConfirmPassword]) {
+        errors[RegisterFormKeys.ConfirmPassword] = 'Passwords do not match';
+    }
+
+    return errors;
+};
 
 export default function Register() {
-
     const { registerSubmitHandler } = useContext(AuthContext);
-    const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+    const { values, errors, submitError, onChange, onSubmit } = useForm(registerSubmitHandler, {
         [RegisterFormKeys.Username]: '',
         [RegisterFormKeys.Email]: '',
         [RegisterFormKeys.Password]: '',
         [RegisterFormKeys.ConfirmPassword]: '',
-    });
+    }, validate);
+
+    const errorStyle = {
+        color: 'rgb(153, 6, 6)',
+        marginTop: '0.5em',
+        fontSize: '40px',
+        textDecoration: 'underline',
+    };
 
     return (
         <div className={styles.registerContainer}>
-              <div className={styles.container}>
-                <h1>Register</h1> 
+            <div className={styles.container}>
+                <h1>Register</h1>
                 <form onSubmit={onSubmit}>
+                    {submitError && <p style={errorStyle}>{submitError}</p>}
                     <div className={styles.formGroup}>
                         <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" name={RegisterFormKeys.Username} placeholder="Username.." onChange={onChange} value={values[RegisterFormKeys.Username]}/>
+                        <input
+                            type="text"
+                            id="username"
+                            name={RegisterFormKeys.Username}
+                            placeholder="Username.."
+                            onChange={onChange}
+                            value={values[RegisterFormKeys.Username]}
+                        />
+                        {errors[RegisterFormKeys.Username] && <p style={errorStyle}>{errors[RegisterFormKeys.Username]}</p>}
                     </div>
 
                     <div className={styles.formGroup}>
                         <label htmlFor="email">Email:</label>
-                        <input type="text" id="email" name={RegisterFormKeys.Email} placeholder="Email.." onChange={onChange} value={values[RegisterFormKeys.Email]}/>
+                        <input
+                            type="text"
+                            id="email"
+                            name={RegisterFormKeys.Email}
+                            placeholder="Email.."
+                            onChange={onChange}
+                            value={values[RegisterFormKeys.Email]}
+                        />
+                        {errors[RegisterFormKeys.Email] && <p style={errorStyle}>{errors[RegisterFormKeys.Email]}</p>}
                     </div>
 
                     <div className={styles.formGroup}>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name={RegisterFormKeys.Password} placeholder="Password.." onChange={onChange} value={values[RegisterFormKeys.Password]}/>
+                        <input
+                            type="password"
+                            id="password"
+                            name={RegisterFormKeys.Password}
+                            placeholder="Password.."
+                            onChange={onChange}
+                            value={values[RegisterFormKeys.Password]}
+                        />
+                        {errors[RegisterFormKeys.Password] && <p style={errorStyle}>{errors[RegisterFormKeys.Password]}</p>}
                     </div>
 
                     <div className={styles.formGroup}>
                         <label htmlFor="rePassword">Confirm Password:</label>
-                        <input type="password" id="rePassword" name={RegisterFormKeys.ConfirmPassword} placeholder="Repeat Password.." onChange={onChange} value={values[RegisterFormKeys.ConfirmPassword]}/>
+                        <input
+                            type="password"
+                            id="rePassword"
+                            name={RegisterFormKeys.ConfirmPassword}
+                            placeholder="Repeat Password.."
+                            onChange={onChange}
+                            value={values[RegisterFormKeys.ConfirmPassword]}
+                        />
+                        {errors[RegisterFormKeys.ConfirmPassword] && <p style={errorStyle}>{errors[RegisterFormKeys.ConfirmPassword]}</p>}
                     </div>
 
                     <input type="submit" value="Submit" className={styles.submitButton} />
-                    
                 </form>
 
                 <p>Already have an account? <Link to="/login">Login here</Link></p>
             </div>
         </div>
-    )
+    );
 }
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,34 +1,33 @@
 import { useState } from "react";
 
-export default function useForm(submitHandler, initialValues) {
+export default function useForm(submitHandler, initialValues, validate) {
     const [values, setValues] = useState(initialValues);
     const [errors, setErrors] = useState({});
     const [submitError, setSubmitError] = useState('');
 
     const onChange = (e) => {
-        setValues(state => ({
+        setValues((state) => ({
             ...state,
             [e.target.name]: e.target.value
         }));
-        setErrors(state => ({ ...state, [e.target.name]: '' }));
+        setErrors((state) => ({ ...state, [e.target.name]: '' })); 
     };
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        const validationErrors = {};
-        
-        
-        if (!values.email) validationErrors.email = 'Email is required';
-        if (!values.password) validationErrors.password = 'Password is required';
 
+     
+        const validationErrors = validate(values);
         if (Object.keys(validationErrors).length === 0) {
             try {
                 await submitHandler(values);
+                setSubmitError(''); 
             } catch (error) {
-                setSubmitError('Email or Password is invalid');
+                setSubmitError('Email or Password is invalid'); 
             }
         } else {
             setErrors(validationErrors);
+            setSubmitError('');
         }
     };
 
